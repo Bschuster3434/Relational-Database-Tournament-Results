6aafbbbf3447574f64b3d0dbe24fe795f9870ca6
Brian Schuster <Bschuster@hc1.com>
2015-04-16 05:58:50 -0400

    Added Table Create Statements to tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -6,4 +6,20 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE tournament (
+tournamentId serial Primary Key,
+tournamentName text);
+
+CREATE TABLE registeredPlayer (
+ playerId serial Primary Key,
+ fullName text,
+ tournament serial references tournament(tournamentId));
+ 
+CREATE TABLE match (
+matchId serial Primary Key,
+playerId serial references registeredPlayer(playerId),
+tournamentId serial references tournament(tournamentId),
+round int,
+result text);
+
 
